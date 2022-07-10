58adff1362a5a01b152926c99469a12ee9beb8a9
ktjs7252 <78638160+ktjs7252@users.noreply.github.com>
2022-07-10 23:49:19 +0900

    Update boj1238.cpp

--- 최단거리/boj1238.cpp
+++ 최단거리/boj1238.cpp
@@ -6,24 +6,25 @@ vector<pair<int,int>> adj[1001],adj2[1001];
 int dist[1001];
 int dist2[1001];
 const int INF=987654321;
-priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq; //"거리", "정점" 이 두가지의 인자를 가진 최소힙기반의 우선순위큐에 넣어가며 진행
+
 int from,to,w;
 int answer;
 
 void solve(int *dist,vector<pair<int,int>> *adj)
 {
-	pq.push(make_pair(0, x));
+	pq.push(make_pair(0, x)); //시작지점 거릿값
 	dist[x] = 0; 
 	
 	while(pq.size()){ 
 		int here = pq.top().second;
 		int here_dist = pq.top().first;
 		pq.pop();
-		if(dist[here]!=here_dist) continue;
+		if(dist[here]!=here_dist) continue; // 전체를 보지 않고 내가 봐야할 가중치만 본다.
 		
 		for(pair<int, int> there : adj[here]){
 			int _dist = there.first, _there = there.second;  
-			if(dist[_there] > dist[here] + _dist){
+			if(dist[_there] > dist[here] + _dist){ //가 탐색하려는 이 정점"이 나의 정점 + 그 정점까지의 거리가 더 작다면 갱신
 				dist[_there] = dist[here] + _dist; 
 				pq.push(make_pair(dist[_there], _there));  
 			}  
@@ -37,7 +38,7 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 	
-	fill(dist,dist+1001,INF);
+	fill(dist,dist+1001,INF); //초기화
 	fill(dist2,dist2+1001,INF);
 	
 	cin>>n>>m>>x;
